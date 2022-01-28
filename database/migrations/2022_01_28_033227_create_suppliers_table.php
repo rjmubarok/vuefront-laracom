a33f2748ba1e32d116fd3a5b8d4ec00b5b7c25e4
rjmubarok <mubarokhossen2003@gmail.com>
2022-01-28 09:38:41 +0600

    supplier database create

---
+++ database/migrations/2022_01_28_033227_create_suppliers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('supplier_name');
+            $table->string('supplier_email');
+            $table->string('supplier_phone');
+            $table->string('address');
+            $table->string('area');
+            $table->string('postcode');
+            $table->boolean('is_trashed ')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
