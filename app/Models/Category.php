b963d5f1aecf65c661e0643e51b66ef2c6875cd1
Md Salah Uddin <bdrosan@gmail.com>
2022-01-30 17:49:54 +0600

    role and permission added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,5 +9,5 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'description', 'image'];
+    protected $fillable = ['name', 'slug', 'description', 'image', 'parent_id'];
 }
