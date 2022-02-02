03eaed11c6e4e8cf815823c5e9f3fd78bc314ba6
Md. Salah Uddin <30566025+bdrosan@users.noreply.github.com>
2022-02-02 14:41:36 +0600

    Update 2022_01_30_122126_create_brands_table.php
    add unique to slug

--- database/migrations/2022_01_30_122126_create_brands_table.php
+++ database/migrations/2022_01_30_122126_create_brands_table.php
@@ -16,7 +16,7 @@ class CreateBrandsTable extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->string('image')->nullable();
             $table->boolean('active')->default('1');
