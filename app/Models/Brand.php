b835148b58138cd8f0f03eabe23f46d8db5ecf41
Md. Salah Uddin <30566025+bdrosan@users.noreply.github.com>
2022-02-02 14:49:45 +0600

    Update Brand.php
    slug added and status removed

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'description', 'image','status'];
+    protected $fillable = ['name', 'slug', 'description', 'image'];
 }
