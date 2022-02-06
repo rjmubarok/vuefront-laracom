eac74e251ee1e88eb81136fa915570404c7623de
Md Salah Uddin <bdrosan@gmail.com>
2022-02-06 18:23:14 +0600

    admin template setup

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,10 @@ Route::view('/', 'home');
 Auth::routes();
 
 Route::prefix('admin')->middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('admin.dashboard');
-    });
+    Route::redirect('/', 'admin/dashboard');
+    Route::view('dashboard','admin.dashboard', ['page_title' => 'Dashboard'])->name('admin.dashboard');
+    Route::view('category','admin.category', ['page_title' => 'Category'])->name('admin.category');
+    Route::view('product','admin.product', ['page_title' => 'Product'])->name('admin.product');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
