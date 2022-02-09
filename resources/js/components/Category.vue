020a6872195c7c12b920bb56a1b00190ce1a948b
Md. Salah Uddin <30566025+bdrosan@users.noreply.github.com>
2022-02-10 00:00:40 +0600

    category updated

--- resources/js/components/Category.vue
+++ resources/js/components/Category.vue
@@ -1,39 +1,346 @@
 <template>
-    <div class="card card-body">
-        <h4>Category</h4>
+  <div class="row">
+    <div class="col-md-8">
+      <div class="card card-body">
+        <div class="table-responsive">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th class="text-center">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" />
+                  </div>
+                </th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Slug</th>
+                <th>Order</th>
+                <th class="text-end">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="text-center">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" />
+                  </div>
+                </td>
+                <td>21</td>
+                <td><b>Men clothes</b></td>
+                <td>Men clothes</td>
+                <td>/men</td>
+                <td>1</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" />
+                  </div>
+                </td>
+                <td>2</td>
+                <td><b>Women fashion</b></td>
+                <td>Fashions for Women</td>
+                <td>/women</td>
+                <td>2</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" />
+                  </div>
+                </td>
+                <td>3</td>
+                <td><b>Kids clothes</b></td>
+                <td>Clothes for kids</td>
+                <td>/kids</td>
+                <td>3</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <i
+                    class="
+                      material-icons
+                      md-subdirectory_arrow_right
+                      text-muted
+                    "
+                  ></i>
+                </td>
+                <td>4</td>
+                <td><b>Hot Gifts</b></td>
+                <td>Hot Gifts</td>
+                <td>/gifts</td>
+                <td>4</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                      aria-expanded="false"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu" style="margin: 0px">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <i
+                    class="
+                      material-icons
+                      md-subdirectory_arrow_right
+                      text-muted
+                    "
+                  ></i>
+                </td>
+                <td>5</td>
+                <td><b>Electronics</b></td>
+                <td>Electronics</td>
+                <td>/electr</td>
+                <td>5</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" />
+                  </div>
+                </td>
+                <td>6</td>
+                <td><b>Accessories</b></td>
+                <td>Accessories</td>
+                <td>/accessories</td>
+                <td>6</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" />
+                  </div>
+                </td>
+                <td>7</td>
+                <td><b>Jewellery</b></td>
+                <td>Jewellery</td>
+                <td>/jewel</td>
+                <td>7</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" />
+                  </div>
+                </td>
+                <td>8</td>
+                <td><b>Interiors</b></td>
+                <td>Interiors</td>
+                <td>/interior</td>
+                <td>8</td>
+                <td class="text-end">
+                  <div class="dropdown">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      class="btn btn-light rounded btn-sm font-sm"
+                    >
+                      <i class="material-icons md-more_horiz"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">View detail</a>
+                      <a class="dropdown-item" href="#">Edit info</a>
+                      <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                  </div>
+                  <!-- dropdown //end -->
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <ul :class="{ loading }">
-            <li v-for="category in categories" :key="category.id">
-                {{ category.name }}
-            </li>
+          <li v-for="category in categories" :key="category.id">
+            {{ category.name }}
+          </li>
         </ul>
+      </div>
     </div>
+    <div class="col-md-4">
+      <div class="card card-body">
+        <form method="post">
+          <div class="mb-4">
+            <label for="name" class="form-label">Category Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              name="name"
+              placeholder="Type here"
+            />
+          </div>
+          <div class="mb-4">
+            <label for="parent" class="form-label">Parent Category</label>
+            <select name="parent" id="parent" class="form-select">
+              <option value="">Select Parent Category</option>
+              <option
+                v-for="category in categories"
+                :key="category.id"
+                :value="category.id"
+              >
+                {{ category.name }}
+              </option>
+            </select>
+          </div>
+          <div class="mb-4">
+            <label class="form-label" for="description">Description</label>
+            <textarea
+              placeholder="Type here"
+              class="form-control"
+              id="description"
+              name="description"
+            ></textarea>
+          </div>
+          <div class="d-grid">
+            <button class="btn btn-primary">Create category</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data: function () {
-        return {
-            loading: true,
-            categories: [],
-        };
-    },
+  data: function () {
+    return {
+      loading: true,
+      categories: [],
+    };
+  },
 
-    mounted() {
-        this.loadCategories();
-    },
+  mounted() {
+    this.loadCategories();
+  },
 
-    methods: {
-        loadCategories: function () {
-            axios
-                .get("/api/category")
-                .then((response) => {
-                    this.categories = response.data;
-                    this.loading = false; //should set at the end all data fetching
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        },
+  methods: {
+    addCategory: function () {},
+    loadCategories: function () {
+      axios
+        .get("/api/category")
+        .then((response) => {
+          this.categories = response.data;
+          this.loading = false; //should set at the end all data fetching
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
+  },
 };
 </script>
